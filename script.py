649927c0e876eeb027e93a8220c529ec9c01d245
Japheth Balane <japheth162@gmail.com>
2017-03-26 01:42:13 +0800

    Added json object structures

--- script.py
+++ script.py
@@ -1,6 +1,46 @@
-backbone = {
-	"faculties": [],
-	"news": []
+
+######################################################################################
+
+mainFormat = {
+	"faculties": [], # based on personFormat
+	"news": []       # based on newsFormat
 }
 
-print backbone
+personFormat = {
+	"name": "",
+	"college": "",
+	"position": "",
+	"photo": "",
+	"introduction": "",
+	"research": [], # based on researchFormat
+	"training": []  # based on trainingFormat
+}
+
+researchFormat = {
+	"title": "",
+	"year-published": 0,
+	"abstract": "",
+	"authors": [], # string array
+	"link": ""
+}
+
+trainingFormat = {
+	"title": "",
+	"date": "",
+	"description": ""
+}
+
+newsFormat = {
+	"title": "",
+	"tags": [], # string array
+	"date": "",
+	"body": "",
+	"author": "",
+	"photo": ""
+}
+
+######################################################################################
+
+print main
+
+######################################################################################
