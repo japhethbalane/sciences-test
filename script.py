0e0640aa6359dcb62c0d1cf4367d2c50dccee7fd
Japheth Balane <japheth162@gmail.com>
2017-03-26 01:56:11 +0800

    Added initialization functions

--- script.py
+++ script.py
@@ -1,46 +1,71 @@
 
+###################################################################################### global variable definitions
+
+mainFormat = {}
+personFormat = {}
+researchFormat = {}
+trainingFormat = {}
+newsFormat = {}
+
+###################################################################################### initializations
+
+def initMain():
+	return {
+		"faculties": [], # based on personFormat
+		"news": []       # based on newsFormat
+	}
+def initPerson():
+	return {
+		"name": "",
+		"college": "",
+		"position": "",
+		"photo": "",
+		"introduction": "",
+		"research": [], # based on researchFormat
+		"training": []  # based on trainingFormat
+	}
+def initResearch():
+	return {
+		"title": "",
+		"year-published": 0,
+		"abstract": "",
+		"authors": [], # string array
+		"link": ""
+	}
+def initTraining():
+	return {
+		"title": "",
+		"date": "",
+		"description": ""
+	}
+def initNews():
+	return {
+		"title": "",
+		"tags": [], # string array
+		"date": "",
+		"body": "",
+		"author": "",
+		"photo": ""
+	}
+
+mainFormat = initMain()
+personFormat = initPerson()
+researchFormat = initResearch()
+trainingFormat= initTraining()
+newsFormat = initNews()
+
 ######################################################################################
 
-mainFormat = {
-	"faculties": [], # based on personFormat
-	"news": []       # based on newsFormat
-}
-
-personFormat = {
-	"name": "",
-	"college": "",
-	"position": "",
-	"photo": "",
-	"introduction": "",
-	"research": [], # based on researchFormat
-	"training": []  # based on trainingFormat
-}
-
-researchFormat = {
-	"title": "",
-	"year-published": 0,
-	"abstract": "",
-	"authors": [], # string array
-	"link": ""
-}
-
-trainingFormat = {
-	"title": "",
-	"date": "",
-	"description": ""
-}
-
-newsFormat = {
-	"title": "",
-	"tags": [], # string array
-	"date": "",
-	"body": "",
-	"author": "",
-	"photo": ""
-}
+######################################################################################
 
 ######################################################################################
 
-print main
+######################################################################################
+
+print mainFormat
+print personFormat
+print researchFormat
+print trainingForma
+print newsFormat
 
 ######################################################################################
